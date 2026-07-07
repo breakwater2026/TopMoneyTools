61741c8a6537ebf2317b6954ce2ea742b4026bd0
breakwater2026 <breakwater2026@gmail.com>
2026-07-07 12:49:49 -0400

    refactoring Vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   resolve: {
