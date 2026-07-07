c17be97114424301f0b15784ed924191955102c5
breakwater2026 <breakwater2026@gmail.com>
2026-07-07 11:56:44 -0400

    edge browser cache fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    historyApiFallback: true
+  }
+});
 
 export default defineConfig({
   resolve: {
